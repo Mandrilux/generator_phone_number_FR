f75ff58cc0c8d4ecb1729ad8f7fa90cee148f9fa
epitech2020 <baptiste.heraud@epitech.eu>
2016-06-15 10:19:46 +0200

    goto_first_version

--- gen.java
+++ gen.java
@@ -2,9 +2,22 @@ public class gen
 {
     public static void main(String[] args)
     {
-	int number[] = new int [10];
-
+	int	number[] = new int [10];
+	int	i, z;
+	
 	init_num(number);
+	i = number.length;
+	while (i-- > 2)
+	    {
+		z = -1;
+		while (++z < 9)
+		    {
+			number[i] += 1;
+			display_num(number);
+		    }
+		number[i] = 0;
+	    }
+	
 	
     }
 
@@ -15,5 +28,15 @@ public class gen
 	while (++i < number.length)
 	    number[i] = 0;
 	number[1] = 6;
+    }
+
+    public static void display_num(int number[])
+    {
+	int	i = -1;
+
+	while (++i < number.length)
+	    System.out.print(number[i]);
+	System.out.println("");
+	
     }
 }
