7c21633995aea659994b47283bec05ac81477dbc
epitech2020 <baptiste.heraud@epitech.eu>
2016-06-15 10:43:33 +0200

    end_prog

--- gen.java
+++ gen.java
@@ -2,38 +2,27 @@ public class gen
 {
     public static void main(String[] args)
     {
-	int	number[] = new int [10];
-	int	i, z;
-	
-	init_num(number);
-	i = number.length;
-	while (i-- > 2)
+	long	number = -1;
+	int	flag = 6;
+	while (flag < 8)
 	    {
-		z = -1;
-		while (++z < 9)
-		    {
-			number[i] += 1;
-			display_num(number);
-		    }
-		number[i] = 0;
-	    }	
+		while (++number <= 99999999)
+		    display_num(number, flag);
+		number = -1;
+		flag++;
+	    }
     }
-
-    public static void init_num(int number[])
+   
+    public static void display_num(long num, int flag)
     {
-	int	i = -1;
-
-	while (++i < number.length)
-	    number[i] = 0;
-	number[1] = 6;
-    }
-
-    public static void display_num(int number[])
-    {
-	int	i = -1;
-
-	while (++i < number.length)
-	    System.out.print(number[i]);
-	System.out.println("");
+	int	len = String.valueOf(num).length();
+	
+	System.out.print("0"+flag);
+	while (len < 8)
+	    {
+		System.out.print("0");
+		len++;
+	    }
+	System.out.println(num);
     }
 }
